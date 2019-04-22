51e96adb0635c5e3fcec3e80317514f2c04b14af
Jay Tong <jayktong@gmail.com>
2019-04-22 15:13:22 -0700

    Update schema design

--- database/database.js
+++ database/database.js
@@ -3,24 +3,6 @@ const sequelize = require('./index.js');
 
 const { Model } = Sequelize;
 
-class Restaurant extends Model {}
-Restaurant.init({
-  id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    unique: true,
-  },
-  link: {
-    type: Sequelize.STRING,
-    allowNull: true,
-  },
-}, {
-  sequelize,
-  modelName: 'restaurant',
-  timestamps: false,
-});
-
 class Menu extends Model {}
 Menu.init({
   id: {
@@ -28,31 +10,21 @@ Menu.init({
     allowNull: false,
     primaryKey: true,
     unique: true,
-    autoIncrement: true,
   },
-  dishtype: {
+  dishes: {
+    type: Sequelize.ARRAY(Sequelize.TEXT),
+    allowNull: true,
+  },
+  link: {
     type: Sequelize.STRING,
-  },
-  subtype: {
-    type: Sequelize.STRING,
-  },
-  dish: {
-    type: Sequelize.STRING,
-  },
-  price: {
-    type: Sequelize.INTEGER,
-  },
-  ingredients: {
-    type: Sequelize.STRING,
+    allowNull: true,
   },
 }, {
   sequelize,
   modelName: 'menu',
   timestamps: false,
+  underscored: true,
 });
-
-Restaurant.hasMany(Menu);
-Menu.belongsTo(Restaurant);
 
 sequelize.sync();
 
