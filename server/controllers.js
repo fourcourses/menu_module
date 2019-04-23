5b780bf02e07a4188c1c0dbaebd3769dafe1368d
Jay Tong <jayktong@gmail.com>
2019-04-23 10:11:42 -0700

    Optimize query speed for GET menus

--- server/controllers.js
+++ server/controllers.js
@@ -1,8 +1,11 @@
 const menus = require('../database/Menu');
+const sequelize = require('../database/index');
 
 exports.getMenu = (req, res) => {
-  menus.findOne({ where: { id: req.params.id } })
-    .then((menu) => {
+  console.time('getMenuTimeLength');
+  const query = `SELECT * FROM menus WHERE id = ${req.params.id};`;
+  sequelize.query(query, { type: sequelize.QueryTypes.SELECT })
+    .then(([menu, metadata]) => {
       const dishesObj = [];
       menu.dishes.forEach((dish) => {
         const dishObj = {
@@ -17,26 +20,32 @@ exports.getMenu = (req, res) => {
       menu.dishes = dishesObj;
       res.status(200);
       res.send(menu);
+      console.timeEnd('getMenuTimeLength');
     })
     .catch((err) => {
       res.status(400);
       res.send(err);
+      console.timeEnd('getMenuTimeLength');
     });
 };
 
 exports.createMenu = (req, res) => {
+  console.time('createMenuTimeLength');
   menus.findOrCreate({ where: { id: req.body.id }, defaults: { dishes: req.body.dishes, link: req.body.link } })
     .then(([menu, created]) => {
       console.log(menu);
       res.send(created);
+      console.timeEnd('createMenuTimeLength');
     })
     .catch((err) => {
       res.status(400);
       res.send(err);
+      console.timeEnd('createMenuTimeLength');
     });
 };
 
 exports.updateMenu = (req, res) => {
+  console.time('updateMenuTimeLength');
   menus.findOne({ where: { id: req.params.id } })
     .then((menu) => {
       if (menu) {
@@ -45,20 +54,25 @@ exports.updateMenu = (req, res) => {
           link: req.body.link,
         });
       }
+      console.timeEnd('updateMenuTimeLength');
     })
     .catch((err) => {
       res.status(400);
       res.send(err);
+      console.timeEnd('updateMenuTimeLength');
     });
 };
 
 exports.deleteMenu = (req, res) => {
+  console.time('deleteMenuTimeLength');
   menus.destroy({ where: { id: req.params.id } })
     .then(() => {
       res.send('Menu deleted');
+      console.timeEnd('deleteMenuTimeLength');
     })
     .catch((err) => {
       res.status(400);
       res.send(err);
+      console.timeEnd('deleteMenuTimeLength');
     });
 };
