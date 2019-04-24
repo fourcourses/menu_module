e3fe5f1e85ef23f13a451cb8f4870e9c7807f0bc
Jay Tong <jayktong@gmail.com>
2019-04-23 20:29:05 -0700

    Create data generation file for Cassandra db

---
+++ cassandradb/datageneration.js
@@ -0,0 +1,40 @@
+const faker = require('faker');
+const fileRestaurant = require('fs').createWriteStream('cassandradb/restaurant.txt');
+// use the line below if you want to generate a smaller test file
+// const fileRestaurant = require('fs').createWriteStream('cassandradb/restaurantShort.txt');
+
+const dishTypes = ['Breakfast', 'Lunch', 'Dinner', 'Brunch', 'Bar', 'Dessert', 'Cheese', 'Craft Cocktails & Libations', 'Afternoon Bites', 'After Dinner', 'Wines by the Glass'];
+const subTypes = ['Mains', 'Starters', 'Sides', 'Raw Bar', 'Snacks', 'Appetizers', 'Classics', 'Recommendations', 'Entrees', 'Small Plates', 'Soups & Salads'];
+
+const generateRestaurants = async () => {
+  fileRestaurant.write('id|dishes|link\n');
+
+  for (let id = 1; id <= 1e7; id += 1) {
+    let link = null;
+    const numberOfDishes = Math.floor(Math.random() * 10 + 10);
+    const linkOrMenu = Math.floor(Math.random() * 3); // 1/3rd chance of menu being a link to menu
+
+    const dishes = [];
+    if (linkOrMenu === 0) {
+      link = faker.internet.url();
+    } else {
+      for (let dishCount = 0; dishCount < numberOfDishes; dishCount += 1) {
+        const dish = {
+          dishType: dishTypes[Math.floor(Math.random() * dishTypes.length)],
+          subType: subTypes[Math.floor(Math.random() * subTypes.length)],
+          dish: faker.lorem.word(),
+          price: parseInt(Math.random() * 85 + 15, 10),
+          ingredients: faker.lorem.words(3),
+        };
+
+        dishes.push(dish);
+      }
+    }
+
+    if (!fileRestaurant.write(`${id}|${JSON.stringify(dishes)}|${link}\n`)) {
+      await new Promise(resolve => fileRestaurant.once('drain', resolve));
+    }
+  }
+};
+
+generateRestaurants();
